acae2dd1732dd5e0412b3b940400d6fae9298fc0
aguchim <73450862+aguchim@users.noreply.github.com>
2021-01-20 02:34:20 +0900

    이진탐색(재귀함수)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,11 +2,23 @@
 #include <string>
 #include <list>
 #include <random>
-void swap(int* i, int* j);
+
+
 void Sort1(int index[], int count);
+void swap(int* i, int* j);
 int binarysearch(int index[], int size, int count);
+int binarysearch(int index[], int Min, int Max, int midle, int count);
+void mainBinarySearch();
+
 
 int main(void)
+{
+	mainBinarySearch();
+	return 0;
+}
+
+
+void mainBinarySearch()
 {
 	int Index[100];
 
@@ -18,22 +30,29 @@ int main(void)
 
 	for (int i = 0; i < sizeof(Index) / sizeof(int); i++)
 	{
+		std::cout << "[";
+		std::cout << i;
+		std::cout << "]";
 		std::cout << Index[i];
 		std::cout << "\n";
 	}
 
 	std::cout << "리스트 탐색 : ";
-	int select = 450;
+	int select = 966;
 	std::cout << select;
-	std::cout << "\n";
+	std::cout << "\n 일반 : ";
+	//일반
 	std::cout << binarysearch(Index, sizeof(Index) / sizeof(int), select);
+	//재귀
+	std::cout << "\n 재귀 : ";
+	std::cout << binarysearch(Index, 0, sizeof(Index) / sizeof(int) - 1, (sizeof(Index) / sizeof(int) / 2), select);
 }
 
+//일반
 int binarysearch(int index[], int size,int count)
 {
 	int first = 0 , last = size -1 ,midle = (first+last)/2;
-
-	while (first >= last)
+	while (first <= last)
 	{
 		midle = (first + last) / 2;
 		if (index[midle] == count)
@@ -52,6 +71,34 @@ int binarysearch(int index[], int size,int count)
 		}
 	}
 	return -1;
+}
+//재귀
+int binarysearch(int index[], int Min, int Max, int midle, int count)
+{
+	//std::cout << "[최대 : ";
+	//std::cout << Max;
+	//std::cout << "] [최소 : ";
+	//std::cout << Min;
+	//std::cout << "] [중간 : ";
+	//std::cout << midle;
+	//std::cout << "] \n";
+
+	if (index[midle] == count)
+	{
+		return count;
+	}
+	else if (Min >= Max)
+	{
+		return -1;
+	}
+	else if (index[midle] > count)
+	{
+		binarysearch(index, Min, midle - 1, (Min + midle - 1) / 2, count);
+	}
+	else if (index[midle] < count)
+	{
+		binarysearch(index, midle + 1, Max, (Max + midle + 1) / 2, count);
+	}
 }
 
 //선택 정렬
